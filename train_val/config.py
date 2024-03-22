b01292fcb5bd81dba536d785282bae4598fd9e83
Huy Hoàng Nguyễn <cavangmauxanh@gmail.com>
2024-03-22 10:22:03 +0700

    cavang

--- train_val/config.py
+++ train_val/config.py
@@ -15,7 +15,7 @@ config = {
         "warmup_steps": 250,
     },
     "model": {
-        "type": 'vit_t',
+        "type": "vit_t",
         "checkpoint": "pretrained_weights/tinysam.pth",
         "freeze": {
             "image_encoder": True,
